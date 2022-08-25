cea6f51a481e7d05774b921cc95c32839858b960
jambin <108569685+dev-jambin@users.noreply.github.com>
2022-08-25 23:10:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner badYear = new Scanner(System.in);
+        int A = badYear.nextInt();
+
+        System.out.println(A - 543);
+    }
+}
