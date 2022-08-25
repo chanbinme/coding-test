90ae8d643a30da86be02da1afb5c0d1f4ab8b612
jambin <108569685+dev-jambin@users.noreply.github.com>
2022-08-25 22:37:06 +0900

    [Bronze V] Title: A/B, Time: 240 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double A;
+		double B;
+		
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextDouble();
+		B = scan.nextDouble();
+		
+		
+		System.out.println(A/B);
+		
+	}
+
+}
