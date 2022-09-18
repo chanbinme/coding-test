e84c6f35d97e85fa76aa222ba6545a9a0e5128c4
jambin <108569685+dev-jambin@users.noreply.github.com>
2022-09-18 13:33:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 596 ms, Memory: 110444 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,18 +1,24 @@
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String z = br.readLine(); // 첫번째 입력 받고 안쓸거임
+        String sr = br.readLine(); // 문자 입력
+        
+        String[] arr = sr.split(" "); // 공백 기준으로 문자열을 분리하여 문자형 배열에 선언
+        int[] intArr = new int[arr.length]; // arr 길이만큼의 정수형 배열 생성
+        
+        for (int i = 0; i < arr.length; i++) {
+            intArr[i] = Integer.parseInt(arr[i]); // arr 배열의 값을 정수형으로 변환하여 intArr에 선언
         }
-        int min = Arrays.stream(arr).min().getAsInt();
-        int max = Arrays.stream(arr).max().getAsInt();
+        
+        int min = Arrays.stream(intArr).min().getAsInt(); // intArr에서 가장 큰 값 호출
+        int max = Arrays.stream(intArr).max().getAsInt(); // intArr에서 가장 작은 값 호출
 
-        System.out.println(min + " " + max);
+        bw.write(min + " " + max); // min, max 출력
+        bw.close(); // buffered 닫기
     }
 }
