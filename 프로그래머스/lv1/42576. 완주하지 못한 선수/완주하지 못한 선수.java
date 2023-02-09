88931cffe7b3710d07c347b258e9617689dd2c09
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2023-02-09 22:14:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.12 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,29 @@
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        /**
+         * 1. 단 한 명만 마라톤을 완주하지 못합니다.
+         * 2. completion의 길이는 participant의 길이보다 1 작습니다.
+         * 3. 참가자 중 동명이인이 있을 수 있습니다.
+         */
         String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String player : participant) 
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        for (String player : completion) 
-            map.put(player, map.get(player) - 1);
+        Map<String, Integer> map = new HashMap<>();
 
-            Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        /* 중복된 선수 이름이 있을 수 있기 때문에 +1 */
+        for (String s1 : participant) map.put(s1, map.getOrDefault(s1, 0) + 1);
 
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if (entry.getValue() != 0){
+        /* 완주한 선수의 value는 0, 완주하지 못한 선수의 value는 1 */
+        for (String s2 : completion) map.put(s2, map.get(s2) - 1);
+
+        /* Map을 순회하여 value가 1인 key를 조회 */
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(1)) {
                 answer = entry.getKey();
-                break;
             }
         }
+
         return answer;
     }
 }
