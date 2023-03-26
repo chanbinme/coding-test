3b1a264c2dda8e63591eb090465f32d2ade6988e
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2023-03-26 13:26:58 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,4 +1,4 @@
-select floor(price / 10000) * 10000 as price_group, count(product_id) as products
+select floor(price / 10000) * 10000 as price_group, count(*) as products
 from product
-group by floor(price / 10000) * 10000
-order by price_group 
+group by price_group
+order by price_group
