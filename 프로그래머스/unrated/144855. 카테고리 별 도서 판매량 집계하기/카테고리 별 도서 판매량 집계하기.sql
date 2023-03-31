79f62dec8f5934257b24ac340c28c2124c707b80
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2023-03-31 10:13:01 +0900

    [unrated] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,11 +1,8 @@
 select b.category as category, sum(bs.sales) as total_sales
 from book b
-left join (
-    select *
-    from book_sales
-    where Year(sales_date) = 2022
-    and Month(sales_date) = 1
-    ) as bs
+left join book_sales bs
 on b.book_id = bs.book_id
+where Year(bs.sales_date) = 2022
+and Month(bs.sales_date) = 1
 group by category
 order by b.category
