c7b1951d2f7104b871c6740f77c5c825cc23d13a
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2023-05-28 23:37:03 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        int index1 = 0; // 카드 1의 인덱스
+        int index2 = 0; // 카드 2의 인덱스
+
+        for(int i=0;i<goal.length;i++){
+            if(cards1[index1].equals(goal[i])){  
+                index1 = (index1 < cards1.length-1)? index1 + 1 : index1;   
+            }else if(cards2[index2].equals(goal[i])){    
+                index2 = (index2 < cards2.length-1)? index2 + 1 : index2;
+            }else{  
+               return "No";
+            }
+        }
+
+        return "Yes";
+    }
+}
