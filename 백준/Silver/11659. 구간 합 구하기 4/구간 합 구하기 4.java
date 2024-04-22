766792f70f9f51b44c113a026fab8e38917df02f
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2024-04-22 23:40:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 692 ms, Memory: 61004 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,36 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader bufferedReader =
-                new BufferedReader(new InputStreamReader(System.in));
+        // 데이터의 개수, 질의 개수
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
 
-        StringTokenizer stringTokenizer =
-                new StringTokenizer(bufferedReader.readLine());
+        // 구간 합을 구할 대상 배열
+        String[] arr = br.readLine().split(" ");
+        int[] sumArr = new int[arr.length];
+        sumArr[0] = Integer.parseInt(arr[0]);
 
-        int count = Integer.parseInt(stringTokenizer.nextToken());
-        int number = Integer.parseInt(stringTokenizer.nextToken());
-
-        int[] arr = new int[count + 1];
-        int[] sumArr = new int[count + 1];
-
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        for (int i = 1; i <= count; i++) {
-            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
-            sumArr[i] = (sumArr[i - 1] + arr[i]);
+        for (int i = 1; i < arr.length; i++) {
+            sumArr[i] = sumArr[i - 1] + Integer.parseInt(arr[i]);
         }
 
-        for (int k = 0; k < number; k++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            int answer = sumArr[j] - sumArr[i - 1];
-            System.out.println(answer);
+        // 구간 합 계산 및 출력
+        for (int i = 0; i < M; i++) {
+            String ij = br.readLine();
+            String[] IJ = ij.split(" ");
+
+            int I = Integer.parseInt(IJ[0]) - 1;
+            int J = Integer.parseInt(IJ[1]) - 1;
+
+            // 첫번째부터 구간합을 구할 경우 계산 필요 없음
+            if (I == 0) {
+                bw.write(sumArr[J] + "\n");
+            } else {
+                bw.write(sumArr[J] - sumArr[I - 1] + "\n");
+            }
         }
+
+        bw.flush();
+        bw.close();
     }
 }
