f6913c92a2c4e9f0c80de885187ee28e63a878e3
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2024-07-26 22:43:18 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45644 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,33 @@
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 1; i <= n; i++) {
+        // N 받음
+        int N = Integer.parseInt(br.readLine());
+
+        // 1~N 만큼 큐에 입력
+        for (int i = 1; i <= N; i++) {
             queue.add(i);
         }
 
+        // 큐 사이즈가 1이 될 때까지 반복
         while (queue.size() > 1) {
+            // 큐에서 가장 앞숫자 삭제
             queue.poll();
+
+            // 그 다음 숫자를 뽑아 다시 큐에 입력
             queue.add(queue.poll());
         }
 
-        System.out.println(queue.poll());
+        assert queue.peek() != null;
+        bw.write(String.valueOf(queue.peek()));
+        bw.flush();
+        bw.close();
     }
 }
