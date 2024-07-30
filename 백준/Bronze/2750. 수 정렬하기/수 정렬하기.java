0649143794707c20c022d092764cae1f0e01e069
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2024-07-30 22:07:15 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 15188 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,20 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int caseSize = Integer.parseInt(br.readLine());
-        int[] ints = new int[caseSize];
-        for (int i = 0; i < caseSize; i++) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] ints = new int[N];
+
+        for (int i = 0; i < N; i++) {
             ints[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] result = Arrays.stream(ints).sorted().toArray();
-        for (int i : result) {
-            System.out.println(i);
+        for (int i = 0; i < N - 1; i++) {
+            // 한 번 돌 때마다 배열 마지막 한칸씩 정렬되었기 때문에 i만큼 빼줌
+            for (int j = 0; j < N - 1 - i; j++) {
+                int preNum = ints[j];
+                int nextNum = ints[j + 1];
+                // 배열 값보다 오른쪽 배열의 값이 더 작으면 값 바꾸기
+                if (preNum > nextNum) {
+                    ints[j] = nextNum;
+                    ints[j + 1] = preNum;
+                }
+            }
         }
+
+        for (int num : ints) {
+            bw.write(String.valueOf(num));
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
+
