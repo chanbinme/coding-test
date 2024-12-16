ab36ea096714cd4b3b5733a244c4989053c33109
Chanbin Kim <108569685+chanbinme@users.noreply.github.com>
2024-12-16 22:59:04 +0900

    refactor - [인프런 코테] Title: 중복문자제거

--- 인프런코테/6. 중복문자제거/Main.java
+++ 인프런코테/6. 중복문자제거/Main.java
@@ -1,6 +1,4 @@
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,27 +13,16 @@ public class Main {
     }
 
     public String solution(String word) {
-        // 문자 리스트로 변환
-        List<Character> chars = word.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-
-        // 마지막 인덱스에 도달할 때까지 반복
-        int idx = 0;
-        while (idx < chars.size() - 1) {
-            // 현재 인덱스의 문자 저장
-            char currentChar = chars.get(idx);
-
-            // 현재 마지막 인덱스부터 인덱스 +1 인덱스 문자까지 반복
-            // 뒤에서부터 루프하는 이유는 중복되는 문자가 있을 경우 삭제하기 위함
-            for (int i = chars.size() - 1; i > idx; i--) {
-                // 중복되는 문자가 있으면 삭제
-                if (currentChar == chars.get(i)) {
-                    chars.remove(i);
-                }
+        String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            // 단어에서 첫번째로 발견되는 문자 인덱스
+            int firstCharIndex = word.indexOf(word.charAt(i));
+            // 단어에서 첫번째로 발견된 문자 인덱스와 현재 인덱스의 값이 같지 않으면 중복된 문자
+            if (firstCharIndex == i) {
+                answer += word.charAt(i);
             }
-            idx++;
         }
-        StringBuilder sb = new StringBuilder();
-        chars.forEach(sb::append);
-        return sb.toString();
+
+        return answer;
     }
 }
